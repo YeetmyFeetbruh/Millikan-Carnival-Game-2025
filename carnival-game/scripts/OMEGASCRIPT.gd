a93033c37393a83cf2c348323c0a82104acc3009
GittingItOnWithYourMom <146044625+GittingItOnWithYourMom@users.noreply.github.com>
2025-10-12 17:10:09 -0700

    changed icon
    didnt have time for anything else

--- carnival-game/scripts/OMEGASCRIPT.gd
+++ carnival-game/scripts/OMEGASCRIPT.gd
@@ -3,3 +3,6 @@ extends Node2D
 # why no code in my omegascript
 
 # fail and win functions in knife.gd should be moved here and called with signals
+
+func _ready():
+	print("you have 10 seconds to carve a perfect cirlce into the pumpkin or the room will fill with GYAS")
